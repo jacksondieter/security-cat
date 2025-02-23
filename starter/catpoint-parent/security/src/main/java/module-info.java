d2743454a3be5f894e7ab1ad3d4dbd7f521f3be6
Dieter Jackson <jacksondieter@gmail.com>
2025-02-23 18:10:02 +0100

    udasecurity: Refactor modules

---
+++ starter/catpoint-parent/security/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.udacity.catpoint.security {
+    requires com.udacity.catpoint.image;
+    requires com.miglayout.swing;
+    requires java.desktop;
+    requires com.google.common;
+    requires com.google.gson;
+    requires java.prefs;
+}
