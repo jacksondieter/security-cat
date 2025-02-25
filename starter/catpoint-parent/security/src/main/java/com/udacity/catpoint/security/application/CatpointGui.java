bd52a5f880949d604f83907f80a9d539eda7a057
Dieter Jackson <jacksondieter@gmail.com>
2025-02-25 19:20:20 +0100

    udasecurity: Add fix maven package

--- starter/catpoint-parent/security/src/main/java/com/udacity/catpoint/security/application/CatpointGui.java
+++ starter/catpoint-parent/security/src/main/java/com/udacity/catpoint/security/application/CatpointGui.java
@@ -1,5 +1,6 @@
 package com.udacity.catpoint.security.application;
 
+import com.udacity.catpoint.image.service.ImageService;
 import com.udacity.catpoint.security.data.PretendDatabaseSecurityRepositoryImpl;
 import com.udacity.catpoint.security.data.SecurityRepository;
 import com.udacity.catpoint.image.service.FakeImageService;
@@ -16,7 +17,7 @@ import javax.swing.*;
  */
 public class CatpointGui extends JFrame {
     private SecurityRepository securityRepository = new PretendDatabaseSecurityRepositoryImpl();
-    private FakeImageService imageService = new FakeImageService();
+    private ImageService imageService = new FakeImageService();
     private SecurityService securityService = new SecurityService(securityRepository, imageService);
     private DisplayPanel displayPanel = new DisplayPanel(securityService);
     private ControlPanel controlPanel = new ControlPanel(securityService);
